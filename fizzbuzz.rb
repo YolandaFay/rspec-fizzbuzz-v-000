bb91849f88368dd1a7534fabe6edcee75fd3c996
Yolanda Fay <yolandafay@gmail.com>
2018-07-28 03:09:21 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,15 +3,18 @@
 
 def fizzbuzz(num)
   
-  if 
-    num % 3 == 0
+  if num % 3 == 0
     return "Fizz"
-  elseif
-    num % 5 == 0
+  end 
+  
+  if num % 5 == 0
     return "Buzz"
-  elseif
+  end 
+  if
     num % 3 == 0 && num % 5 == 0
     return "FizzBuzz"
+  end
+  
   else
     puts nil
   end
