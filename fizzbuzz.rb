bb39a127aa3b3e63da657c7d1bea4604fabfbae0
Yolanda Fay <yolandafay@gmail.com>
2018-07-28 02:57:00 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,15 @@
 
 def fizzbuzz(num)
   
-  if num/3 == 
+  if 
+    num % 3 == 0
+    puts "Fizz"
+  elseif
+    num % 5 == 0
+    puts "Buzz"
+  elseif
+    (num % 3 == 0) && (num % 5 == 0)
+    puts "FizzBuzz"
   
   
   
